b95add6bed22b8f5704c05111eb555ce6df6230f
Raphael Pinheiro <raphaelpfarias@gmail.com>
2019-01-07 13:59:57 -0200

    improving EntryFormComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 
@@ -17,6 +18,7 @@ import { InMemoryDatabase } from './in-memory-database'
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase) // Responsavel por interceptar a requisição HTTP in memory. Necessário retirar para utilização de servidor real.
   ],
   providers: [],
